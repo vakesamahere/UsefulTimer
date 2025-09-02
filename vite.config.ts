e9b588f93b3118ea73c5be8c303d049084e8ff89
vakesamahere <vakegamer@163.com>
2025-09-02 21:54:54 +0800

    feat: add @types/uuid dependency and update vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      '*',
+      'ztt.aimixtech.com'
+    ],
+  },
 })
